0141f12125407f7fe5988409ea4489ca83b64e99
issac-kondreddy <ixk93660@ucmo.edu>
2024-02-08 16:53:39 -0600

    Few GUI components

---
+++ Medium level Java/src/GUI/Checkbox/Main.java
@@ -0,0 +1,12 @@
+package GUI.Checkbox;
+
+public class Main{
+
+    public static void main(String[] args) {
+
+        // JCheckBox = A GUI component that can be selected or deselected
+
+        new MyFrame();
+
+    }
+}
---
+++ Medium level Java/src/GUI/window/main.java
@@ -0,0 +1,10 @@
+package GUI.window;
+
+public class main {
+
+    public static void main(String[] args) {
+
+        Launchpage launchPage = new Launchpage();
+
+    }
+}
